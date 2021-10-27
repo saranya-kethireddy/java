7d6941bb4b81936932c20db93980f6f7271df081
skethireddy2 <skethireddy2@IN-PG02LCPA>
2021-10-27 16:23:22 -0700

    unitest example

---
+++ java/src/unittests/TestJunitTestCase.java
@@ -0,0 +1,43 @@
+package unittests;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestJunitTestCase extends TestCase {
+	
+	 JunitTestCaseExample obj = new JunitTestCaseExample();  
+     
+	    @Test  
+	    public void testAdd() {  
+	        obj.add("Emma");  
+	        obj.add("Ronan");  
+	        obj.add("Antonio");  
+	        obj.add("Paul");  
+	        assertEquals("Adding 4 student to list", 4, obj.sizeOfStudent());  
+	    }  
+	      
+	    @Test  
+	    public void testSize() {  
+	        obj.add("Emma");  
+	        obj.add("Ronan");  
+	        obj.add("Antonio");  
+	        assertEquals("Checking size of List", 3, obj.sizeOfStudent());  
+	    }  
+	      
+	    @Test  
+	    public void testRemove() {  
+	        obj.add("Antonio");  
+	        obj.add("Paul");  
+	        obj.remove("Paul");  
+	        assertEquals("Removing 1 student from list", 1, obj.sizeOfStudent());  
+	    }  
+	      
+	    
+		@Test  
+	    public void removeAll() {  
+	        obj.removeAll();  
+	    }  
+
+
+}
