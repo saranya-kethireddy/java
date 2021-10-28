713dbd1fbdbd9ea059cee81bc04e78ffe85a272a
skethireddy2 <skethireddy2@IN-PG02LCPA>
2021-10-28 00:42:37 -0700

    file outputstream

---
+++ java/src/streams/FileOutputStreamExample.java
@@ -0,0 +1,19 @@
+package streams;
+
+import java.io.FileOutputStream;
+
+public class FileOutputStreamExample {
+	
+	public static void main(String args[]){    
+	           try{    
+	             FileOutputStream fout=new FileOutputStream("C:\\Users\\skethireddy2\\OneDrive - DXC Production\\Desktop\\testout.txt");    
+	             fout.write(65);    
+	             fout.close();    
+	             System.out.println("success...");    
+	            }
+	           catch(Exception e){
+	        	   System.out.println(e);
+	           }    
+	      }    
+	}  
+
